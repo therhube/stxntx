1e6a6d5f4a54becacf07502ffe08e5701c4dedb1
adamrhuberg <adamrhuberg@upwork.com>
2016-06-05 12:45:24 -0700

    updated CSS; copied a function to child theme in order to hide comments from homepage tiles

--- wordpress/wp-content/themes/baskerville-child/functions.php
+++ wordpress/wp-content/themes/baskerville-child/functions.php
@@ -10,4 +10,32 @@ function theme_enqueue_styles() {
     );
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+
+function baskerville_child_meta() { ?>
+<!-- AR: added this function simply to comment out the comment count link; don't need a bunch of zeroes everywhere -->
+
+	<div class="post-meta">
+	
+		<a class="post-date" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_time( 'Y m d' ); ?></a>
+		
+		<?php
+		
+			if( function_exists('zilla_likes') ) zilla_likes(); 
+		
+			/*if ( comments_open() ) {
+				comments_popup_link( '0', '1', '%', 'post-comments' );
+			}
+			*/
+			edit_post_link(); 
+		
+		?>
+		
+		<div class="clear"></div>
+	
+	</div> <!-- /post-meta -->
+	
+<?php
+}
+
 ?>
